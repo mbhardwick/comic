c1623a236307efd3200d970210ad4583c5765555
Naldo Tumundo <naldo_j3@yahoo.com>
2019-11-06 19:09:29 -0800

    got html routes working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,21 @@
-var db = require("../models");
+console.log('HTML Route Connected Successfully');
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+
+// Node Dependencies
+var path = require('path');
+
+
+// Includes  Routes
+function htmlRoutes(app) {
+
+
+  // A default USE route that leads to home.html which displays the home page.
+  app.use(function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/index.html'));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+}
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
-};
+
+// Export for use in main server.js file
+module.exports = htmlRoutes;
